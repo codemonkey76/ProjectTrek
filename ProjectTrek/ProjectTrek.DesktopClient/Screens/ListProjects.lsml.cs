10073b27792a1264fb4248ed5cbd6418e2bf5992
Shane Poppleton <shane@bjja.com.au>
2014-02-05 14:52:14 +1000

    Enabling and Disabling Methods

---
+++ ProjectTrek/ProjectTrek.DesktopClient/Screens/ListProjects.lsml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+namespace LightSwitchApplication
+{
+    public partial class ListProjects
+    {
+        partial void CreateNewProject_Execute()
+        {
+            this.Projects.AddNew();
+            this.FindControl("Projects_SelectedItem_Code").Focus();
+        }
+
+        partial void CloseProject_Execute()
+        {
+            this.Projects.SelectedItem.Closed = true;
+        }
+
+        partial void ReopenProject_Execute()
+        {
+            this.Projects.SelectedItem.Closed = false;
+        }
+
+        partial void CloseProject_CanExecute(ref bool result)
+        {
+            result = !this.Projects.SelectedItem.Closed;
+        }
+
+        partial void ReopenProject_CanExecute(ref bool result)
+        {
+            result = this.Projects.SelectedItem.Closed;
+        }
+    }
+}
