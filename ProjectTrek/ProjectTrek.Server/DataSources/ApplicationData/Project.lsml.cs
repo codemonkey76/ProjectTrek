a5cd38bbfc09f43c8fc46ccbe4ef5597f5c978ed
Shane Poppleton <shane@bjja.com.au>
2014-02-05 12:33:55 +1000

    Added Computed Fields

---
+++ ProjectTrek/ProjectTrek.Server/DataSources/ApplicationData/Project.lsml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class Project
+    {
+        partial void TotalTaskHours_Compute(ref int result)
+        {
+            result = this.ProjectTasks.Sum(t => t.EstimatedHours);
+        }
+
+        partial void TotalAdditionalCosts_Compute(ref decimal result)
+        {
+            result = this.AdditionalCosts.Sum(c => c.Amount);
+        }
+    }
+}
